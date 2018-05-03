bbe211ee57a70cfb0cbd86f50c129e47877eeb2a
Frank Pfattheicher <fpf@ict-baden.de>
2018-05-03 14:14:09 +0200

    work in progress...

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # EchoBelkinDevice
 Emulated Belkin WeMo devices that work with the Amazon Echo
+
+
+## Links
+
+https://github.com/kakopappa/arduino-esp8266-alexa-multiple-wemo-switch
+https://github.com/makermusings/fauxmo
---
+++ TestDevice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using DeviceEmulator;
+
+namespace TestDevice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var device = new SwitchDevice("Test", 1234, IPAddress.Parse("192.168.2.121"), 8100);
+
+            var upnp = new UpnpBroadcastResponder();
+            upnp.AddDevice(device);
+
+            Console.ReadLine();
+        }
+    }
+}
